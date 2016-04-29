c04c51ab726c603ef71532ad70d633655e847ab2
Eric Le Bihan <eric.le.bihan.dev@free.fr>
2016-04-29 20:33:01 +0200

    test: introduce Test and Suite classes

--- famulus/test.py
+++ famulus/test.py
@@ -29,6 +29,7 @@
 """
 
 from enum import Enum
+from .log import debug
 from gettext import gettext as _
 
 BASE_KEYS = ['name', 'type', 'category', 'author', 'brief', 'description']
@@ -73,5 +74,94 @@ class SuiteSpec(BaseSpec):
     """
     def __init__(self, params):
         BaseSpec.__init__(self, params, SUITE_KEYS)
+
+
+class BaseTest:
+    """Base class for test/suite.
+
+    @param name: name of the object
+    @type name: str
+    """
+    def __init__(self, name):
+        self._name = name
+        self.author = _('Unknown')
+        self.brief = _('No brief description available')
+        self.description = _('No description available')
+
+    @property
+    def name(self):
+        return self._name
+
+    def run(self):
+        raise NotImplementedError
+
+
+class Test(BaseTest):
+    """Represent a test to run.
+
+    @param name: name of test
+    @type name: str
+
+    @param command: command to execute
+    @type command: str
+
+    @param expect: expected text
+    @type expect: str
+    """
+    def __init__(self, name, command, expect):
+        BaseTest.__init__(self, name)
+        self._command = command
+        self._expect = expect
+        self.setup = []
+        self.teardown = []
+
+    def run(self):
+        """Run the test"""
+        debug(_("Running test {}").format(self.name))
+
+
+class Suite(BaseTest):
+    """Represent a suite to run.
+
+    @param name: name of suite
+    @type name: str
+    """
+    def __init__(self, name):
+        BaseTest.__init__(self, name)
+        self._tests = []
+        self._suites = []
+
+    @property
+    def tests(self):
+        return self._tests
+
+    @property
+    def suites(self):
+        return self._suites
+
+    def add_test(self, test):
+        """Add a test to the suite.
+
+        @param test: a test to add to the suite
+        @type test: Test
+        """
+        self._tests.append(test)
+
+    def add_suite(self, suite):
+        """Add a suite to the suite.
+
+        @param suite: a suite to add to the suite
+        @type suite: Suite
+        """
+        self._suites.append(suite)
+
+    def run(self):
+        """Run all the tests, then all the suites"""
+        debug(_("Running suite {}").format(self.name))
+        for test in self._tests:
+            test.run()
+        for suite in self._suites:
+            suite.run()
+
 
 # vim: ts=4 sw=4 sts=4 et ai
