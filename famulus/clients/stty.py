c0a67ca0e444fbf8d0b727917d7f1097560bafc5
Eric Le Bihan <eric.le.bihan.dev@free.fr>
2016-07-06 22:02:34 +0200

    clients: introduce STTYClient
    Add STTYClient, a client which interacts with a target connected via a
    serial TTY to the host.

---
+++ famulus/clients/stty.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of famulus
+#
+# Copyright (C) 2016 Eric Le Bihan <eric.le.bihan.dev@free.fr>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+"""
+   famulus.clients.stty
+   ````````````````````
+
+   Classes and helper functions for interacting with a machine via a serial
+   TTY.
+
+   :copyright: (C) 2016 Eric Le Bihan <eric.le.bihan.dev@free.fr>
+   :license: GPLv3+
+"""
+
+from .serial import SerialClient
+
+
+class STTYClient(SerialClient):
+    """Client which interacts with a machine via a serial TTY"""
+    def __init__(self, address):
+        SerialClient.__init__(self, address)
+        self.shell_prompt = r'[\w]+@\([\w]+\):~# '
+        self.login_prompt = r'\([\w]+\) login: '
+        self.passw_prompt = 'Password: '
+
+# vim: ts=4 sw=4 sts=4 et ai
